38c3d9fb809141852920d86306e3e81821d76ced
AlexandrSapunov <59012092+AlexandrSapunov@users.noreply.github.com>
2023-04-22 14:15:16 +0800

    Get request

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            using (StreamReader strr = new StreamReader(HttpWebRequest.Create(@"http://biik.ru/rasp/cg109.htm").GetResponse().GetResponseStream()))
+                str = strr.ReadToEnd();
+            Console.Write(str);
+            Console.ReadKey();
+        }
+    }
+}
