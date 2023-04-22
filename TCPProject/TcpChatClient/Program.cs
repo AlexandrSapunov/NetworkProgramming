1989688ba319aaed4c463902fb05335d309b5093
AlexandrSapunov <59012092+AlexandrSapunov@users.noreply.github.com>
2023-04-22 14:06:23 +0800

    Lab2 TCP protocol

---
+++ TCPProject/TcpChatClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Enter ip");
+            string ip = Console.ReadLine();
+            int port = 8080;
+            Client.Client client = new Client.Client(ip,port,name);
+            client.Connect();
+
+            while (true)
+            {
+                Console.WriteLine("Enter cmd");
+                string cmd = Console.ReadLine();
+                switch (cmd)
+                {
+                    case "msg":
+                        Console.Write("Msg:");
+                        client.SendMessage(Console.ReadLine());
+                        break;
+                    case "file":
+                        Console.Write("Enter path:");
+                        string path = Console.ReadLine();
+                        string msg = "file " + File.ReadAllBytes(path);
+                        client.SendMessage(msg);
+                        break;
+                }
+                
+            }
+        }
+    }
+}
---
+++ TCPProject/TcpChatServer/Server/Server.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TcpChatServer.Server
+{
+    public class Server
+    {
+        public ServerInfo HostInfo = new ServerInfo();
+        private TcpListener tcpListener;
+        private Thread thread;
+
+        public bool IsEnable { get; private set; }
+        public static List<Client> Clients { get; private set; }
+
+
+        public Server(string ip,int port)
+        {
+            if (Clients == null)
+                Clients = new List<Client>();
+            HostInfo.Port = port;
+            thread = new Thread(LoopGetData);
+            //IPAddress ipAddres = IPAddress.Parse(HostInfo.Ip);
+            //IPEndPoint ipHostEndPoint = new IPEndPoint(ipAddres, HostInfo.Port);
+            tcpListener = new TcpListener(IPAddress.Any,HostInfo.Port);
+            HostInfo.Ip = tcpListener.LocalEndpoint;
+        }
+
+        private void LoopGetData()
+        {
+            while (IsEnable)
+            {
+                try
+                {
+                    var clientSocket = tcpListener.AcceptTcpClient();
+                    var client = new Client(clientSocket);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Error connection: {ex.Message}");
+                    break;
+                }
+            }
+        }
+
+        public void SendMessage(string msg)
+        {
+            foreach (var client in Clients)
+                client.SendMessage("Server", msg);
+        }
+
+        public void Start()
+        {
+            IsEnable = true;
+            tcpListener.Start();
+            thread.Start();
+            Console.WriteLine($"Server is started on {HostInfo.Ip}");
+        }
+
+        public void Stop()
+        {
+            IsEnable = false;
+        }
+
+    }
+}
