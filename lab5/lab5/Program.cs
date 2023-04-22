ee148afe8e00b4578bc439d22ab32fae21d21596
AlexandrSapunov <59012092+AlexandrSapunov@users.noreply.github.com>
2023-04-22 14:22:51 +0800

    FTP download file

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Lab5
+{
+    class Program
+    {
+        public class WebRequestGetExample
+        {
+            public static void Main(string[] args)
+            {
+                WebClient client = new WebClient();
+                client.Credentials = new NetworkCredential("username", "password");
+                client.DownloadFile("https://www.interestprograms.ru/content/files/sources/tcpsendfiles2_vs11.zip", "file.zip");
+                Console.WriteLine("Operation completed!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
