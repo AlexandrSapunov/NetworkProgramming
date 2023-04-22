cbe88c0b21c5bfd9a0913258ec033763e5284e58
AlexandrSapunov <59012092+AlexandrSapunov@users.noreply.github.com>
2023-04-22 14:11:21 +0800

    lab 1 output PC information

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string myHost;
+            myHost = Dns.GetHostName();
+            Console.WriteLine($"Имя компьютера: {myHost}");
+            IPAddress address = Dns.GetHostAddresses(myHost).First<IPAddress>(f => f.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+            if (address != null)
+            {
+                Console.WriteLine($"Адрес: {address} Семейство: {address.AddressFamily}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
